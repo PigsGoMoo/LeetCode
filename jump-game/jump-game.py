18c821ffdd22dc836d282dd302f745d013532d65
Johny Dang <33019839+PigsGoMoo@users.noreply.github.com>
2021-05-05 00:34:46 -0400

    Time: 92 ms (36.91%), Space: 16.2 MB (51.55%) - LeetHub

---
+++ jump-game/jump-game.py
@@ -0,0 +1,14 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        # Initialize variable to keep track of our jump
+        current_jump = 0
+                
+        # Loop through array
+        for i in range(len(nums) - 1):
+            # Check the next furthest jump
+            current_jump = max(current_jump, i + nums[i])
+            
+            if i == current_jump and i != len(nums) - 1:
+                return False
+            
+        return True
