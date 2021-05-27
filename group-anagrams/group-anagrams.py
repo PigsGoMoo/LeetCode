34af0e10e7bd2e5342f31868b9ab17710f91acf1
Johny Dang <33019839+PigsGoMoo@users.noreply.github.com>
2021-05-27 10:29:14 -0400

    Time: 216 ms (5.02%), Space: 19.8 MB (16.35%) - LeetHub

--- group-anagrams/group-anagrams.py
+++ group-anagrams/group-anagrams.py
@@ -3,9 +3,32 @@ class Solution:
         # Default dict makes it so when you make a key, there will be a "default" value pair for it already
         # Useful if you want your value pair to be an array but don't want to have to initialize the array
         # first.
+#         ans = collections.defaultdict(list)
+        
+#         for s in strs:
+#             ans[tuple(sorted(s))].append(s)
+            
+#         return ans.values()
         ans = collections.defaultdict(list)
+    
+    
+        def getKey(counter):
+            alphabet = "abcdefghijklmnopqrstuvwxyz"
+            res = ""
+            for letter in alphabet:
+                res += 'letter{}'.format(counter.get(letter, 0))
+                
+            return res
+        
         
         for s in strs:
-            ans[tuple(sorted(s))].append(s)
+            counter = {}
+            
+            for char in s:
+                counter[char] = counter.get(char, 0) + 1
+            
+            key = getKey(counter)
+            
+            ans[key].append(s)
             
         return ans.values()
