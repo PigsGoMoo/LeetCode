1410596af7e0040c69e3153f24933b07170860fe
Johny Dang <33019839+PigsGoMoo@users.noreply.github.com>
2021-06-02 18:24:20 -0400

    Time: 2664 ms (5.01%), Space: 43.3 MB (68.52%) - LeetHub

---
+++ time-needed-to-inform-all-employees/time-needed-to-inform-all-employees.py
@@ -0,0 +1,15 @@
+class Solution:
+    def numOfMinutes(self, n: int, headID: int, manager: List[int], informTime: List[int]) -> int:
+        # Helper func for dfs
+        def dfs(i):
+            return max([dfs(j) for j in children[i]] or [0]) + informTime[i]
+        
+        
+        children = [[] for _ in range(n)]
+        
+        for i, m in enumerate(manager):
+            if m >= 0:
+                children[m].append(i)
+                
+        return dfs(headID)
+        
